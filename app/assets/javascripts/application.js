a9d79b70b056276574759f5a826739a77d50a1dd
belette06 <cyril.avronsart@gmail.com>
2019-02-04 02:13:57 +0100

    add js scrollinkclick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,46 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).ready(function(){
+
+		var scrollLink = $('.scroll');
+    
+	scrollLink.click(function(event){
+	    event.preventDefault(); //prevents default jump to section.
+	    $('body,html').animate({
+	      scrollTop: $(this.hash).offset().top
+	    }, 1000);
+	 });
+
+
+
+
 
 $(window).scroll(function() {
 
+	var scrollLink = $('.scroll');
+     var scrollBarLocation = $(this).scrollTop();
+ 
+	 
     if ($(window).scrollTop() > 900) {
-        $('nav').addClass('fixed-top');
-
-        
-    } else {
+        $('nav').addClass('fixed-top');        
+    } 
+    else {
         $('nav').removeClass('fixed-top');
-            }
+    }
+
+    scrollLink.each(function(){
+
+      //creates a variable for the offset from the window top to the section with the link id
+      var  sectionOffset = $(this.hash).offset().top-30;
+
+      //if scrolled further/equal to the id of the link, add active class to anchor parent(li) and remove active class from anchor parent siblings (other li). Else remove active class,
+      if(sectionOffset <= scrollBarLocation){
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    });
+ 
+});
 
 });
